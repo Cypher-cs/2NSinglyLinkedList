38b8f354f57ee030c9c024d7f68208af0879c4ac
prajjwal <prajjwal199275@gmail.com>
2021-02-04 17:12:08 +0530

    implement some fields and constructor in MySinglyLinkedList class

--- src/implementation/MySinglyLinkedList.java
+++ src/implementation/MySinglyLinkedList.java
@@ -1,5 +1,15 @@
 package implementation;
 
-public class MySinglyLinkedList {
+import myinterface.SinglyLinkedListADT;
 
+public class MySinglyLinkedList implements SinglyLinkedListADT {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MySinglyLinkedList(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
 }
